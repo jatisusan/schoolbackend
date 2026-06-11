85593fc2f723aeb95c7add6cb2cfda6f10877010
jatisusan <susurrus112@gmail.com>
2026-06-11 10:21:01 +0545

    Init

--- src/handlers/course.controller.js
+++ src/handlers/course.controller.js
@@ -7,9 +7,30 @@ const getAllCourses = async (req, res) => {
     .json({ message: "All courses fetched successfully", data: courses });
 };
 
-const findCourseById = async (req, res) => {};
+const findCourseById = async (req, res) => {
+  const { id } = req.params;
+  const course = await prisma.course.findUnique({
+    where: { id: Number(id) },
+  });
+  if (!course) {
+    return res.status(404).json({ message: "Course not found" });
+  }
+  res.status(200).json({ message: "Course found successfully", data: course });
+};
 
-const createCourse = async (req, res) => {};
+const createCourse = async (req, res) => {
+  const { name, credits, departmentId } = req.body;
+  const course = await prisma.course.create({
+    data: {
+      name,
+      credits,
+      departmentId,
+    },
+  });
+  res
+    .status(201)
+    .json({ message: "Course created successfully", data: course });
+};
 
 const updateCourse = async (req, res) => {};
 
